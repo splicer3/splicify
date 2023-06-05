e1fd269e1661fa6ff44e3ef580c61a80d937673e
Matteo Di Iorio <matteodiio941@gmail.com>
2023-06-05 12:47:43 +0200

    Bugfix

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -16,11 +16,14 @@ const MediaItem: React.FC<MediaItemProps> = ({
     onClick,
 }) => {
     const imageUrl = useLoadImage(data);
+    const player = usePlayer();
 
     const handleClick = () => {
         if (onClick) {
             return onClick(data.id);
         }
+
+        return player.setId(data.id)
     }
 
     return (
